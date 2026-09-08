adf2e650d18a30fcbcf1fe37e3097617b48f8c87
siannmmmdov-cell <siannmmmdov@gmail.com>
2026-09-08 12:13:34 +0400

    Remove duplicate GIF URL in sex command

--- bot.py
+++ bot.py
@@ -401,8 +401,7 @@ async def clear_cmd(ctx, amount: int = 5):
 @bot.command(name="sex", aliases=["fuck", "kiss", "öpüş", "öp"])
 async def sex_cmd(ctx, member: discord.Member):
     gifler = [
-        "https://media1.giphy.com/media/3o7TKGhH4CKKBlOWtW/giphy.gif",
-        "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHp1aXl5NXZ4OWFpOTZwbGJqeGZ4aWJpOW5ueDRqaXlnZWMyNXVxMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2vn7_xvqT1VwY/giphy.gif",
+        "https://media.giphy.com/media/2vn7_xvqT1VwY/giphy.gif",
         "https://media.giphy.com/media/12VXIxGaNRFdlm/giphy.gif",
         "https://media.giphy.com/media/l4FGuhL4U2WyjdkaY/giphy.gif",
         "https://media.giphy.com/media/3og0IPxMM0erATueVW/giphy.gif"
@@ -507,4 +506,3 @@ if __name__ == "__main__":
     if TOKEN:
         bot.run(TOKEN)
         
- 
